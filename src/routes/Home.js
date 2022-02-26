da40537a16c7f4033c925af0cd17fa187b6a4907
jinsunkim <jinsunkim94@gmail.com>
2022-02-26 23:32:30 +0900

    Fetch data with graphql

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CHARACTERS = gql`
+  query {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
 const Home = () => {
+  let { loading, error, data } = useQuery(GET_CHARACTERS);
+  console.log(loading);
+  console.log(error);
+  console.log(data);
   return <div>Home</div>;
 };
 export default Home;
