8dadbde228b0aef9bdfa64483bc812abb4dac99e
jinsunkim <jinsunkim94@gmail.com>
2022-03-10 23:43:20 +0900

    Modify home page layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import ErrorPage from "./components/404";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Detail />} />
-        <Route path="/404" element={<ErrorPage/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 const Characters = ({ id, name, image, species }) => {
   return (
-    <div className="text-center hover:scale-110 ease-in duration-200 mx-5 my-5">
+    <div className="text-center hover:scale-110 ease-in duration-200 mx-5 my-5 ">
       <Link className=" no-underline"to={`/${id}`} id={id}>
-      <img className="w-full border-solid border-5 rounded-full  mx-auto my-0" src={image} alt={name} />
+      <img className="w-full border-solid border-5 rounded-full  mx-auto my-0 shadow-lg" src={image} alt={name} />
       <p className="text-3xl">{name}</p>
       </Link>
     </div>
